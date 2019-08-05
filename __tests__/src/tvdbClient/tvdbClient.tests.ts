f54a3d14b0409867a997c4ca20c0d34ef0afd0b8
Tim Culleton <mit_notelluc@hotmail.com>
2019-08-05 15:11:15 +1000

    Rough Login and search implementation

---
+++ __tests__/src/tvdbClient/tvdbClient.tests.ts
@@ -0,0 +1,43 @@
+import { TvDBClient } from "../../../src/server/tvdbClient/tvdbClient";
+
+describe(`TVDB Client Tests`, () => {
+
+    let authToken: string;
+    it(`Login to tvdbAPI valid apiKey`, async () => {
+        const tvdbClient = new TvDBClient({
+            apiKey: ``,
+        });
+
+        const data = await tvdbClient.login(`217570E2EAD13858`);
+        expect(data).toBeTruthy();
+        expect(data.token).toBeTruthy();
+        authToken = data.token;
+    });
+
+    it(`Login to tvdbAPI invalid apiKey`, async done => {
+        const tvdbClient = new TvDBClient({
+            apiKey: ``,
+        });
+
+        try {
+            const data = await tvdbClient.login(`xxx-xxxx`);
+            done.fail(`Should have thrown an invalid error`);
+        } catch (e) {
+            expect(e).toBeTruthy();
+            done();
+        }
+    });
+
+    it(`Search Series One Piece`, async () => {
+        const tvdbClient = new TvDBClient({
+            apiKey: ``,
+        });
+
+        const data = await tvdbClient.searchSeries({
+            name: "One Piece",
+        },
+        authToken);
+
+        expect(data).toBeTruthy();
+    });
+});
